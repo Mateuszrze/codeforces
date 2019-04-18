ec4e82b4cd28bb020c59c34b9145d07658b6893e
Mateuszrze <39000168+Mateuszrze@users.noreply.github.com>
2019-04-18 23:53:06 +0200

    Update konwolucje.cpp

--- konwolucje.cpp
+++ konwolucje.cpp
@@ -1,7 +1,11 @@
-const int mod = 1e9 + 7;
+
 const LL inv2 = (mod + 1) / 2LL;
-// f = 0 jak chcemy wyliczyc pierwiastki
-// f = -1 jak chcemy cofnac wyliczanie pierwiastkow
+// testowane na zadaniu https://codeforces.com/contest/914/problem/G
+// f = 1 jak chcemy wyliczyc wartosci w punktach
+// f = -1 jak chcemy cofnac wyliczanie w punktach
+// mozna uzywac fwt_* (co, co2, gdzie bedzie wynik, dlugosc) dziala, gdy co lub co2
+// co i co2 zostaje odwrocone (w postaci wartosci w punktach) - jest tak zeby bylo szybciej
+// funkcje fwt_* nie dziaja gdy dostana te sama tablie (pierwsze przejscie zrobi sie dwa razy)
 
 inline void FWT_or(int *a, int n, int f) {
     for(int len = 1; len < n; len <<= 1) {
@@ -40,4 +44,31 @@ inline void FWT_xor(int *a, int n, int f) {
         }
     }
     return;
+}
+
+void fwt_xor(int *a, int *b, int * c, int n)
+{
+  FWT_xor(a, n, 1);
+  FWT_xor(b, n, 1);
+  for(int i = 0; i < n;i++)
+    c[i] = a[i] * (LL) b[i] % mod;
+  FWT_xor(c, n, -1);
+}
+
+void fwt_and(int *a, int *b, int * c, int n)
+{
+  FWT_and(a, n, 1);
+  FWT_and(b, n, 1);
+  for(int i = 0; i < n;i++)
+    c[i] = a[i] * (LL) b[i] % mod;
+  FWT_and(c, n, -1);
+}
+
+void fwt_or(int *a, int *b, int * c, int n)
+{
+  FWT_or(a, n, 1);
+  FWT_or(b, n, 1);
+  for(int i = 0; i < n;i++)
+    c[i] = a[i] * (LL) b[i] % mod;
+  FWT_or(c, n, -1);
 }
